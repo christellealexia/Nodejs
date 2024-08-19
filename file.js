ecb21d3a43416e11ee89381c0ec43b994ad4eaea
Alexia Christelle <christellealexia53@gmail.com>
2024-08-19 16:54:27 +0200

    Writing in files

--- file.js
+++ file.js
@@ -1,9 +1,16 @@
-
+//Reading files
 const fs = require('fs');
 fs.readFile('./docs/blog1.txt',(err,data) => {
 if(err){
     console.log(err)
 }
-console.log(data.toString());
+console.log(data.toString())
 });
+
+
+
+//Writing files
+fs.writeFile('./docs/blog1.text','Hello world', ()=> {
+    console.log('File written');
+    });
 
