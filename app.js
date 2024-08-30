52451a4a17a1c2289587551dd71ff5dec5fb9fc7
Alexia Christelle <christellealexia53@gmail.com>
2024-08-30 10:43:06 +0200

    Added mongoose to connect to the db

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-const app = express()
+//express app
+ const app = express()
+const dbURI = 'mongodb+srv://chichi:test1234@cluster0.odoac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result)=>app.listen(3000))
+.catch((error) => console.log(error))
+app.set('view engine', 'ejs')
 
-app.listen(3000)
+
+
+// middle ware and static
+app.use(express.static('public'))
+app
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index')
 });
 
-app.get('/about.html', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+app.get('/about', (req, res) => {
+    res.render('about')
 });
 
 app.use((req,res)=> {
-res.status(404).sendFile('./views/404page.html', {root:__dirname})
+res.status(404).render('404')
 })
 
 
